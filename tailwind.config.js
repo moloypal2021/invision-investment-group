3fcb4ebbf6d609f5b22baee15f2953d9f88c4627
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2021-06-24 12:40:20 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize: {
+        "100px": "100px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
